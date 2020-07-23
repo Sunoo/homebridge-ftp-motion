e2cc4645244d2f7d43c8f83f97e08761d8baff08
David Maher <sunookitsune@kitsunet.org>
2020-07-22 22:24:27 -0400

    Cleaned up server logging

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # homebridge-ftp-motion
+
 [![npm](https://img.shields.io/npm/v/homebridge-ftp-motion) ![npm](https://img.shields.io/npm/dt/homebridge-ftp-motion)](https://www.npmjs.com/package/homebridge-ftp-motion)
 
 This plugin converts FTP uploads into HTTP motion alerts [homebridge-camera-ffmpeg](https://github.com/homebridge-plugins/homebridge-camera-ffmpeg) understands.
@@ -7,27 +8,30 @@ Note that this plugin itself does not expose any devices to HomeKit.
 
 This is in an extremely early state, and currently only triggers alerts after uploading an image into the folder with the same name as your camera. The image is not currently stored anywhere.
 
-### Installation
+## Installation
+
 1. Install Homebridge using the [official instructions](https://github.com/homebridge/homebridge/wiki).
 2. Install homebridge-camera-ffmpeg using `sudo npm install -g homebridge-camera-ffmpeg --unsafe-perm`.
 3. Install this plugin using `sudo npm install -g homebridge-ftp-motion`.
 4. Update your configuration file. See configuration sample below.
 
 ### Configuration
+
 Edit your `config.json` accordingly. Configuration sample:
- ```
-    "platforms": [
-        {
-            "platform": "ftpMotion",
-            "ftp_port": "5000",
-            "http_port": 8080,
-            "cameras": [
-                {
-                    "name": "Cat Food Camera",
-                }
-            ]
-        }
-    ]
+
+ ```json
+"platforms": [
+    {
+        "platform": "ftpMotion",
+        "ftp_port": "5000",
+        "http_port": 8080,
+        "cameras": [
+            {
+                "name": "Cat Food Camera",
+            }
+        ]
+    }
+]
 ```
 
 | Fields               | Description                                                                             | Required |
@@ -39,7 +43,9 @@ Edit your `config.json` accordingly. Configuration sample:
 | \|- name             | Name of your camera. (Needs to be the same as in homebridge-camera-ffmpeg config)       | Yes      |
 
 ### Camera Configuration
+
 To use this plugin, you'll need to configure the FTP settings on your camera as listed below. Your camera may use slightly different terms for some of these options.
+
 - `Host Name`: The host name or IP address of the computer running Homebridge
 - `Port`: The value you used for `ftp_port` in the plugin configuration.
 - `Username` and `Password`: Any value can currently be used, as authentication is not currently supported in this plugin. That will likely be added in future versions.
--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const ip = require('ip');
 const stream = require('stream');
 const http = require('http');
 const pathjs = require('path');
+const bunyan = require('bunyan');
 
 module.exports = function(homebridge) {
-    homebridge.registerPlatform("homebridge-ftp-motion", "ftpMotion", ftpMotion, true);
+    homebridge.registerPlatform('homebridge-ftp-motion', 'ftpMotion', ftpMotion, true);
 }
 
 function ftpMotion(log, config, api) {
@@ -15,27 +16,49 @@ function ftpMotion(log, config, api) {
     this.cameraConfigs = config.cameras || [];
     this.httpPort = config.http_port || 8080;
 
-    if (api) {
-        api.on('didFinishLaunching', this.startFtp.bind(this));
-    }
+    api.on('didFinishLaunching', this.startFtp.bind(this));
 }
 
 ftpMotion.prototype.startFtp = function() {
     const ipAddr = ip.address('public', 'ipv4');
     const ftpPort = this.config.ftp_port || 5000;
+    const bunLog = bunyan.createLogger({
+        name: 'ftp',
+        streams: [{ stream: new Logger(this.log), type: 'raw' }]
+    });
     const ftpServer = new FtpSrv({
-        url: `ftp://${ipAddr}:${ftpPort}`,
+        url: 'ftp://' + ipAddr + ':' + ftpPort,
         pasv_url: ipAddr,
         anonymous: true,
-        blacklist: ['MKD', 'APPE', 'RETR', 'DELE', 'RNFR', 'RNTO', 'RMD']
+        blacklist: ['MKD', 'APPE', 'RETR', 'DELE', 'RNFR', 'RNTO', 'RMD'],
+        log: bunLog
     });
     ftpServer.on('login', (data, resolve) => {
         resolve({fs: new MotionFS(data.connection, this), cwd: '/'});
     })
     ftpServer.listen()
     .then(() =>  {
-        this.log(`FTP server started on port ${ftpPort}.`);
+        this.log('FTP server started on port ' + ftpPort + '.');
     })
+}
+
+class Logger {
+    constructor(log) {
+        this.log = log;
+    }
+
+    write(data) {
+        const message = 'FTP Server: ' + data.msg;
+        if (data.level >= 50) {
+            this.log.error(message);
+        } else if (data.level >= 40) {
+            this.log.warn(message);
+        } else if (data.level >= 30) {
+            this.log.info(message);
+        } else if (data.level >= 20) {
+            this.log.debug(message);
+        }
+    }
 }
 
 class MotionFS extends FileSystem {
@@ -73,17 +96,15 @@ class MotionFS extends FileSystem {
 
     chdir(path = '.') {
         path = pathjs.join(this.cwd, path);
-        const pathSplit = path == '..' ? [] : path.split(/\//).filter((value) => value != '');
+        const pathSplit = path.split('/').filter(value => value.length > 0);
         if (pathSplit.length == 0) {
             this.cwd = path;
             return path;
-        } else {
-            if (pathSplit.length == 1) {
-                const camera = this.motion.cameraConfigs.find(camera => camera.name == pathSplit[0]);
-                if (camera) {
-                    this.cwd = path;
-                    return path;
-                }
+        } else if (pathSplit.length == 1) {
+            const camera = this.motion.cameraConfigs.find(camera => camera.name == pathSplit[0]);
+            if (camera) {
+                this.cwd = path;
+                return path;
             }
         }
         this.connection.reply(550, 'No such directory.');
@@ -91,25 +112,22 @@ class MotionFS extends FileSystem {
     }
 
     write(fileName, {append = false, start = undefined}) {
-        var writeStream = stream.Writable();
-        const pathSplit = this.cwd.split(/\//).filter((value) => value != '');
+        const pathSplit = this.cwd.split('/').filter((value) => value != '');
         if (pathSplit.length == 1) {
             const camera = this.motion.cameraConfigs.find(camera => camera.name == pathSplit[0]);
             if (camera) {
-                var started = false;
-                writeStream._write = (chunk, encoding, done) => {
-                    if (!started) {
-                        started = true;
-                        this.motion.log.debug(camera.name + ' Motion Detected!');
-                        try {
-                            http.get(`http://127.0.0.1:${this.motion.httpPort}/motion?${camera.name}`);
-                        } catch (ex) {
-                            this.motion.log.console.error(camera.name + ': Error making HTTP call: ' + ex);
-                        }
-                    }
-                    done();
+                this.motion.log.debug(camera.name + ' Motion Detected!');
+                try {
+                    http.get('http://127.0.0.1:' + this.motion.httpPort + '/motion?' + camera.name);
+                } catch (ex) {
+                    this.motion.log.error(camera.name + ': Error making HTTP call: ' + ex);
                 }
-                return writeStream;
+                var nullStream = stream.Writable({
+                    write: (chunk, encoding, done) => {
+                        done();
+                    }
+                });
+                return nullStream;
             }
         }
         this.connection.reply(550, 'Permission denied.');
